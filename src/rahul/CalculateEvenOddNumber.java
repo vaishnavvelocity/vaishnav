66e00dc665a30ff39e3db6e6ccfacc9e2d2fba0b
User <User@DESKTOP-F2Q23RG>
2021-12-21 17:42:04 +0530

    create new branch and pushh

---
+++ src/rahul/CalculateEvenOddNumber.java
@@ -0,0 +1,22 @@
+package rahul;
+
+public class CalculateEvenOddNumber {
+
+	public static void main(String[] args) {
+		
+		int a=55;
+		
+		if(a%2==0)
+		{
+			System.out.println("number is even");
+		}
+		
+		else
+		{
+			System.out.println("number is odd");
+		}
+			
+		
+	}
+
+}
